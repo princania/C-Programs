f90456ab4188c53770bd3045b5e11f33485e2e54
princania <premmahato7488@gmail.com>
2023-07-15 12:43:29 +0530

    Accepting Command Line arguments

---
+++ CommandLineArguments.c
@@ -0,0 +1,9 @@
+//This program show's how to accept command line arguments.
+
+#include <stdio.h>
+
+void main(int a, char *b[])
+{
+	printf("Number of inputs: %i\n",a);
+	printf("You Entered: %s",b[1]);
+}
