ea4a420873baf91c1168210561def620d36e62d0
princania <premmahato7488@gmail.com>
2023-07-16 12:39:55 +0530

    Binary Search Algorithm

--- binarySearch.c
+++ binarySearch.c
@@ -9,13 +9,19 @@ void main(void)
 	int first = 0;
 	int last = length-1;
 	//Number to be searched
-	int search = 89;
+	int search = 88;
 	
 	int found = 0;
 
 	while(found == 0) //Assuming that the number is definetly present
 	{
 		int middle = (last + first)/2;
+
+		if(middle == first) //Checking if the number is not present in the array, dry run concludes that if the number is not present it will get in a infinite loop where every time it tries to find the next middle index, it will be same as the first index.
+		{
+			found = 1;
+			printf("It is not present in the array");
+		}
 		
 		if(arr[middle] == search)
 		{
